59a40748190c6459237d705ee3a6fd3e0d6db261
tisseragaston <tisseragaston3@gmail.com>
2024-09-25 18:03:48 -0300

    yyy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcodeTerminal = require('qrcode-terminal');
+const qrcode = require('qrcode');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors()); // Habilitar CORS para todas las rutas
 app.use(bodyParser.json()); // Para poder leer el cuerpo de las solicitudes JSON
 
 // Configuración de WhatsApp Web
@@ -12,10 +14,14 @@ const client = new Client({
 });
 
 // Evento para manejar el código QR
-client.on('qr', (qr) => {
-    // Mostrar el código QR en la terminal
-    qrcodeTerminal.generate(qr, { small: true });
-    console.log('QR Code generado. Escanea el código en WhatsApp.');
+client.on('qr', async (qr) => {
+    try {
+        const qrImage = await qrcode.toDataURL(qr);
+        console.log(`QR Code generated: ${qrImage}`);
+        console.log(`Access the QR code at: https://mensajesalus.onrender.com/qr`);
+    } catch (err) {
+        console.error('Error generating QR code:', err);
+    }
 });
 
 // Evento que se dispara cuando el cliente está listo
@@ -25,15 +31,25 @@ client.on('ready', () => {
 
 // Ruta para manejar el envío de mensajes
 app.post('/send-message', (req, res) => {
-    const message = req.body.message; // Toma el mensaje del cuerpo de la solicitud
-    const chatId = '5492645161444@c.us'; // Cambia esto al ID del chat o número de teléfono de destino
+    const message = req.body.message;
+    const chatId = '5492645161444@c.us';
 
     client.sendMessage(chatId, message).then(response => {
         res.json({ message: 'Mensaje enviado' });
     }).catch(err => {
-        console.error(err);
+        console.error('Error al enviar el mensaje:', err);
         res.status(500).json({ error: 'Error al enviar el mensaje' });
     });
+});
+
+// Ruta para acceder al QR en formato de imagen
+app.get('/qr', async (req, res) => {
+    try {
+        const qrImage = await qrcode.toDataURL(qr);
+        res.send(`<img src="${qrImage}" alt="QR Code">`);
+    } catch (err) {
+        res.status(500).send('Error generating QR image.');
+    }
 });
 
 // Inicia el cliente y el servidor
